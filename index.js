99df3d632300e543331be1ea07f5e4d531ea27a5
Shahabr99 <sean.rohani9@gmail.com>
2024-07-06 17:00:18 -0700

    minor style changes

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const navLinks = document.querySelector(".nav-links")
 
 
 openMenu.addEventListener("click", () => {
-    console.log('menu clicked');
     openMenu.classList.add("hidden");
     closeMenu.classList.add("active");
     navLinks.classList.add("active");
 }) 
 
 closeMenu.addEventListener("click", () => {
-    console.log('menu clicked');
     openMenu.classList.remove("hidden");
     closeMenu.classList.remove("active");
     navLinks.classList.remove("active");
